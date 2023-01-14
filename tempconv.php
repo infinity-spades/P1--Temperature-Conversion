503d83eb68fc0d06e28f5d9c0d4f7d88baabeffb
Rich <122461980+richtrimble@users.noreply.github.com>
2023-01-14 06:27:08 -0800

    Update tempconv.php

--- tempconv.php
+++ tempconv.php
@@ -1,34 +1,42 @@
 <?php
 //tempconv.php
 #
-# Initial shot at the app
+# Version 2 updated by Rich & Etsegenet 1/13/22
 #
 if(isset($_POST["TempInput"])){
+	$test = is_numeric($_POST["TempInput"]);
 	
-	$message = "<p>Your temperature is: {$_POST['TempInput']}";
+	if($test) {
 	
-	if($_POST["TypeTemp"]=="fahrenheit") {
-		$message = $message."F (Fahrenheit)</p>";
-		$fTemp = $_POST["TempInput"];
-		$cTemp = (5/9) * ($_POST["TempInput"] - 32);
-		$kTemp = $cTemp + 273.15;
-		echo $message."<p>Celcius is ".$cTemp."C - Kelvin is ".$kTemp."K</p>";
+		echo "Test is ".$test;
+	  	$message = "<p>Your temperature is: {$_POST['TempInput']}";
+	  	
+	  	if($_POST["TypeTemp"]=="fahrenheit") {
+	  		$message = $message."F (Fahrenheit)</p>";
+	  		$fTemp = $_POST["TempInput"];
+	  		$cTemp = number_format((5/9) * ($_POST["TempInput"] - 32),2);
+	  		$kTemp = number_format($cTemp + 273.15,2);
+	  		echo $message."<p>Celcius is ".$cTemp."C - Kelvin is ".$kTemp."K</p>";
+	  	}
+	  	
+	  	if($_POST["TypeTemp"]=="celcius") {
+	  		$message = $message."C (Celcius)</p>";
+	  		$cTemp = $_POST["TempInput"];
+	  		$fTemp = number_format((9/5) * $cTemp + 32,2);
+	  		$kTemp = number_format($cTemp + 273.15,2);
+	  		echo $message."Fahrenheit is ".$fTemp."F - Kelvin is ".$kTemp."K<br><br>";
+	  	}
+	  	
+	  	if($_POST["TypeTemp"]=="kelvin") {
+	  		$message = $message."K (Kelvin)</p>";
+	  		$kTemp = $_POST["TempInput"];
+	  		$cTemp = number_format($kTemp - 273.15,2);
+	  		$fTemp = number_format((9/5) * $cTemp + 32,2);
+	  		echo $message."Fahrenheit is ".$fTemp."F - Celcius is ".$cTemp."C<br><br>";
+	  	}
 	}
-	
-	if($_POST["TypeTemp"]=="celcius") {
-		$message = $message."C (Celcius)</p>";
-		$cTemp = $_POST["TempInput"];
-		$fTemp = (9/5) * $cTemp + 32;
-		$kTemp = $cTemp + 273.15;
-		echo $message."Fahrenheit is ".$fTemp."F - Kelvin is ".$kTemp."K<br><br>";
-	}
-	
-	if($_POST["TypeTemp"]=="kelvin") {
-		$message = $message."K (Kelvin)</p>";
-		$kTemp = $_POST["TempInput"];
-		$cTemp = $kTemp - 273.15;
-		$fTemp = (9/5) * $cTemp + 32;
-		echo $message."Fahrenheit is ".$fTemp."F - Celcius is ".$cTemp."C<br><br>";
+	else {
+		echo "You gotta put in a number";
 	}
 }
 	
